15e5dfb2d1e9f909a07ab5d0c92b2ea4ec0b3c02
josema535 <josema535@gmail.com>
2022-04-11 23:46:07 +0200

    script fin ejercicio 1

--- 01-linux/ejercicio3.sh
+++ 01-linux/ejercicio3.sh
@@ -4,3 +4,9 @@ if [[ $# -gt 1 ]]; then
 else
     TEXTOFICHERO='Que me gusta la bash!!!!'
 fi
+mkdir foo
+mkdir ./foo/dummy/
+mkdir ./foo/empty/
+echo $TEXTOFICHERO > file1.txt 
+touch file2.txt
+echo Fin ejercicio 1
